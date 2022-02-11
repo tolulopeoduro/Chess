55826f3576ac4bec5f6ae74100c157de84bb4088
tolulopeoduro <49287935+tolulopeoduro@users.noreply.github.com>
2022-02-11 14:06:00 +0000

    knight

--- src/utils/checkMoves.js
+++ src/utils/checkMoves.js
@@ -6,6 +6,7 @@ const checkMoves = (data , row , box) => {
     !data && store.dispatch(setAvailableMoves(null))
     data === 'pawn_A' && checkPawnMovesA(data , row , box)
     data === 'pawn_B' && checkPawnMovesB(data , row , box)
+    data.split('_')[0] === 'knight' && checkKnightMoves(data , row , box)
 }
 
 export const clearMoves = store.dispatch(clearAvailableMoves())
@@ -39,6 +40,24 @@ const checkPawnMovesA = (data , row , box) => {
     if(b[row+1][box+1]?.split('_')[1] === 'B') {
         available_moves.push({row : row+1 , box : box+1 })
     }
+    store.dispatch(setAvailableMoves(available_moves))
+}
+
+const checkKnightMoves = (data , row , box ) => {
+    const type = data.split('_')[1]
+    const available_moves = []
+    const b = state.board.board
+    const checkValidity = (r , x) => {
+        if (r > 7 || x > 7 || r < 0 || x < 0) return
+        if(b[r][x] === '' || b[r][x].split('_')[1] !== type) {
+            available_moves.push({row : r , box : x })
+        }
+    }
+    const p = [
+        {row : row-2 , box : box-1} , {row : row-2 , box : box+1} , {row : row+2 , box : box-1} , {row : row+2 , box : box+1},
+        {row : row-1 , box : box+2} , {row : row-1 , box : box-2} , {row : row+1 , box : box+2 , row : row+1 , box : box-2}
+    ]
+    for (let i = 0 ; i < p.length ; i++) {checkValidity(p[i].row , p[i].box)}
     store.dispatch(setAvailableMoves(available_moves))
 }
 
