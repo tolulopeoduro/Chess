22a6f8c6f533959ca4f38e44613da53d5d2c7644
tolulopeoduro <49287935+tolulopeoduro@users.noreply.github.com>
2022-04-07 09:56:14 +0100

    style fixes

--- src/utils/move_animation.js
+++ src/utils/move_animation.js
@@ -14,7 +14,7 @@ const animate_move = (data , selection , time) => {
     const el_topPos = el.getBoundingClientRect().top + window.scrollY;
     const el_leftPos = el.getBoundingClientRect().left + window.scrollX;
     const audio = new Audio('https://res.cloudinary.com/dtuafcbbd/video/upload/v1648606384/chess/game-piece-slide-1-sound-effect.mp3')
-    audio.play()
+    // audio.play()
     $(el).css({
         'transition' : `all ${time}s`,
         'transform' : `translate(${leftPos -el_leftPos}px , ${topPos - el_topPos}px)`
