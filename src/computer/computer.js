aa4a7f5498ae6c77f5bc8d1d7d5d6b00110da579
tolulopeoduro <49287935+tolulopeoduro@users.noreply.github.com>
2022-04-16 08:48:26 +0100

    fix minimax algorithm

--- src/computer/computer.js
+++ src/computer/computer.js
@@ -10,7 +10,8 @@ import minimax from './minimax/minimax'
 const computer = (side ) => {
     
     let best_move = null
-    let best_move_score = 0
+    let best_move_score = null
+    let all_moves = []
 
     const B = store.getState().board.board
     for(let i = 0 ; i < B?.length ; i++) {
@@ -19,10 +20,13 @@ const computer = (side ) => {
                 const moves = checkMoves(B[i][j] , i , j , B)
                 for (let k = 0; k < moves.length ; k++) {
                     const {row , box} = moves[k]
-
                     const selection = {piece : B[i][j] , row : i , box : j }
                     const data = {current_piece : B[row][box] , current_row : row , current_box : box}
+                    all_moves.push({selection , data})
                     const current_score = minimax(setMove(selection , data ) , side)
+                    if (best_move_score === null) {
+                        best_move_score = current_score
+                    }
                     if (current_score > best_move_score ) {
                         best_move_score = current_score
                         best_move = {selection , data}
@@ -31,8 +35,9 @@ const computer = (side ) => {
             }
         }
     }
-    // const {data , selection} = best_move
-    // movePiece(data , null , null , selection)
+        console.log(best_move)
+        const {data , selection} = best_move
+        movePiece(data , null , null , selection)
 
 }
 
